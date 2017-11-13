384b13a838294670660b2dd727db3295bc2716b8
Enrico Pasquali <pasquali.enrico@yahoo.it>
2017-11-13 18:42:38 +0100

    Add FundingController documentation

--- app/src/Controllers/FundingController.php
+++ app/src/Controllers/FundingController.php
@@ -23,6 +23,10 @@ class FundingController extends Controller
     private $sponsorModel;
     private $errorHelper;
 
+    /**
+     * FundingController constructor.
+     * @param \Slim\Container $container
+     */
     public function __construct($container)
     {
         parent::__construct($container);
@@ -32,6 +36,16 @@ class FundingController extends Controller
         $this->sponsorModel = new SponsorModel($this->db, $this->errorHelper);
     }
 
+    /**
+     * Show all the funding.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function showAll(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -50,6 +64,17 @@ class FundingController extends Controller
         ]);
     }
 
+    /**
+     * Create page for a new event.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \PDOException
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function create(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -70,6 +95,16 @@ class FundingController extends Controller
         ]);
     }
 
+    /**
+     * Event creation action.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     */
     public function doCreate(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -93,6 +128,16 @@ class FundingController extends Controller
         return $response->withRedirect($this->router->pathFor('fundings'));
     }
 
+    /**
+     * Edit page for an event.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function edit(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -125,6 +170,16 @@ class FundingController extends Controller
         ]);
     }
 
+    /**
+     * Edit event action.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     */
     public function doEdit(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -153,6 +208,16 @@ class FundingController extends Controller
         return $response->withRedirect($this->router->pathFor('fundings'));
     }
 
+    /**
+     * Delete page for an event.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function delete(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -185,6 +250,16 @@ class FundingController extends Controller
         ]);
     }
 
+    /**
+     * Delete event action.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function doDelete(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -209,16 +284,39 @@ class FundingController extends Controller
         return $response->withRedirect($this->router->pathFor('fundings'));
     }
 
+    /**
+     * Return the all the events with the relative funding in the column
+     * 'finanziamento'.
+     *
+     * @return array The events with them funding.
+     */
     private function getEventsWithFunding(): array
     {
         return $this->eventModel->getEventsWithFunding();
     }
 
+    /**
+     * Return the all the funding of the event identified by $eventID
+     * sponsored by the sponsor identified by $sponsorID.
+     *
+     * @param $eventID int A valid event identifier.
+     * @param $sponsorID int A valid sponsor identifier.
+     * @return array The funding to the event $eventID by the sponsor
+     *         $sponsorID.
+     * @throws \PDOException
+     */
     private function getFunding($eventID, $sponsorID): array
     {
         return $this->fundingModel->getFunding($eventID, $sponsorID);
     }
 
+    /**
+     * Create a new funding using $data as values. The input array is
+     * validated before submitting the query.
+     *
+     * @param $data array The values to insert in the new funding.
+     * @return bool TRUE if the funding is created, FALSE otherwise.
+     */
     private function createFunding($data): bool
     {
         if ($this->checkFundingCheck($data) === false) {
@@ -228,6 +326,16 @@ class FundingController extends Controller
         return $this->fundingModel->createFunding($data);
     }
 
+    /**
+     * Update an already existent funding identified by $eventID and $sponsorID
+     * with the values in $data. The input array is validated before submitting
+     * the query.
+     *
+     * @param $eventID int The event identifier.
+     * @param $sponsorID int The sponsor identifier.
+     * @param $data array The new values.
+     * @return bool TRUE if the funding was updated, FALSE otherwise.
+     */
     private function updateFunding($eventID, $sponsorID, $data): bool
     {
         if ($this->checkFundingCheck($data) === false) {
@@ -237,6 +345,13 @@ class FundingController extends Controller
         return $this->fundingModel->updateFunding($eventID, $sponsorID, $data);
     }
 
+    /**
+     * Delete the event identified by $eventID and $sponsorID.
+     *
+     * @param $eventID int The event identifier.
+     * @param $sponsorID int The sponsor identifier.
+     * @return bool TRUE if the funding was deleted, FALSE otherwise.
+     */
     private function deleteFunding($eventID, $sponsorID): bool
     {
         return $this->fundingModel->deleteFunding($eventID, $sponsorID);
@@ -254,13 +369,21 @@ class FundingController extends Controller
     }
 
     /**
-     * @return array
+     * Return all the sponsor.
+     *
+     * @return array All the sponsor.
      */
     private function getSponsors(): array
     {
         return $this->sponsorModel->getSponsors();
     }
 
+    /**
+     * Validate the values in $data.
+     *
+     * @param $data array The values for the funding.
+     * @return bool TRUE if the values are valid, FALSE otherwise.
+     */
     private function checkFundingCheck(&$data): bool
     {
         $amount = $data['importo'];
