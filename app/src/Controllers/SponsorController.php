95ad0b58b8664478c00aba63fc7a89fc58fc124e
Enrico Pasquali <pasquali.enrico@yahoo.it>
2017-11-13 18:57:29 +0100

    Add SponsorController documentation

--- app/src/Controllers/SponsorController.php
+++ app/src/Controllers/SponsorController.php
@@ -18,6 +18,10 @@ class SponsorController extends Controller
     private $sponsorModel;
     private $errorHelper;
 
+    /**
+     * SponsorController constructor.
+     * @param \Slim\Container $container
+     */
     public function __construct($container)
     {
         parent::__construct($container);
@@ -25,6 +29,16 @@ class SponsorController extends Controller
         $this->sponsorModel = new SponsorModel($this->db, $this->errorHelper);
     }
 
+    /**
+     * Show all the sponsor.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function showAll(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -43,6 +57,16 @@ class SponsorController extends Controller
         ]);
     }
 
+    /**
+     * Create page for a new sponsor.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function create(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -58,6 +82,16 @@ class SponsorController extends Controller
         ]);
     }
 
+    /**
+     * Sponsor creation action.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function doCreate(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -81,6 +115,16 @@ class SponsorController extends Controller
         return $response->withRedirect($this->router->pathFor('sponsors'));
     }
 
+    /**
+     * Edit page for a sponsor.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function edit(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -111,6 +155,16 @@ class SponsorController extends Controller
         ]);
     }
 
+    /**
+     * Edit sponsor action.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function doEdit(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -135,6 +189,16 @@ class SponsorController extends Controller
         return $response->withRedirect($this->router->pathFor('sponsors'));
     }
 
+    /**
+     * Delete page for a sponsor.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function delete(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -165,6 +229,16 @@ class SponsorController extends Controller
         ]);
     }
 
+    /**
+     * Delete sponsor action.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed The rendered page.
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
     public function doDelete(/** @noinspection PhpUnusedParameterInspection */
         Request $request, Response $response, $args)
     {
@@ -184,16 +258,34 @@ class SponsorController extends Controller
         return $response->withRedirect($this->router->pathFor('sponsors'));
     }
 
+    /**
+     * Return all the sponsor.
+     *
+     * @return array All the sponsor.
+     */
     private function getSponsors(): array
     {
         return $this->sponsorModel->getSponsors();
     }
 
+    /**
+     * Return the sponsor identified by $sponsorID.
+     *
+     * @param $sponsorID int A valid sponsor identifier.
+     * @return array The sponsor identified by $sponsorID.
+     */
     private function getSponsor($sponsorID): array
     {
         return $this->sponsorModel->getSponsor($sponsorID);
     }
 
+    /**
+     * Create a new sponsor using $data as values. The input array is
+     * validated before submitting the query.
+     *
+     * @param $data array The values to insert in the new sponsor.
+     * @return bool TRUE if the sponsor is created, FALSE otherwise.
+     */
     private function createSponsor($data): bool
     {
         if ($this->checkSponsorData($data) === false) {
@@ -203,6 +295,15 @@ class SponsorController extends Controller
         return $this->sponsorModel->createSponsor($data);
     }
 
+    /**
+     * Update an already existent sponsor identified by $sponsorID with the
+     * values in $data. The input array is validated before submitting
+     * the query.
+     *
+     * @param $sponsorID int The sponsor identifier.
+     * @param $data array The new values.
+     * @return bool TRUE if the funding was updated, FALSE otherwise.
+     */
     private function updateSponsor($sponsorID, $data): bool
     {
         if ($this->checkSponsorData($data) === false) {
@@ -212,11 +313,23 @@ class SponsorController extends Controller
         return $this->sponsorModel->updateSponsor($sponsorID, $data);
     }
 
+    /**
+     * Delete the event identified by $sponsorID.
+     *
+     * @param $sponsorID int The sponsor identifier.
+     * @return bool TRUE if the funding was deleted, FALSE otherwise.
+     */
     private function deleteSponsor($sponsorID): bool
     {
         return $this->sponsorModel->deleteSponsor($sponsorID);
     }
 
+    /**
+     * Validate the values in $data.
+     *
+     * @param $data array The values for the sponsor.
+     * @return bool TRUE if the values are valid, FALSE otherwise.
+     */
     private function checkSponsorData($data): bool
     {
         $sponsorName = $data['nome'];
